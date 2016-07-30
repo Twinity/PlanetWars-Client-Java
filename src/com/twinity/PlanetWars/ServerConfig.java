c7a25b1ad83f72757d87e31629e6dfc9b86b553b
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 02:09:42 +0430

    Added _serverPort and _clientRetryTimes to ServerConfig

--- src/com/twinity/PlanetWars/ServerConfig.java
+++ src/com/twinity/PlanetWars/ServerConfig.java
@@ -9,6 +9,8 @@ public class ServerConfig {
     private static int _minThreads = 2;
     private static int _maxThreads = 8;
     private static int _timeOutMillis = 3000;
+    private static int _serverPort = 4000;
+    private static int _clientRetryTimes = 5;
 
     public static int getPort() {
         return _port;
@@ -24,6 +26,20 @@ public class ServerConfig {
 
     public static int getTimeout() {
         return _timeOutMillis;
+    }
+
+    public static int getServerPort() {
+        return _serverPort;
+    }
+
+    public static int getClientRetryTimes() {
+        return _clientRetryTimes;
+    }
+
+    public static void setClientRetryTimes(int inTimes) {
+        if (_clientRetryTimes >= 0) {
+            _clientRetryTimes = inTimes;
+        }
     }
 
 }
