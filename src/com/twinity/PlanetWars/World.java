11f0dfc225933ddc2bdc893fceb7a15f0e14759b
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 02:35:49 +0430

    Added getter functions to World class

--- src/com/twinity/PlanetWars/World.java
+++ src/com/twinity/PlanetWars/World.java
@@ -14,4 +14,32 @@ public class World {
     private int currentTurn;
     private int remainingTurns;
 
+    public Node[] getMyNodes() {
+        return myNodes;
+    }
+
+    public Node[] getAllNodes() {
+        return allNodes;
+    }
+
+    public Node[] getOpponentNodes() {
+        return opponentNodes;
+    }
+
+    public Node[] getFreeNodes() {
+        return freeNodes;
+    }
+
+    public int getTotalTurns() {
+        return totalTurns;
+    }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+
+    public int getRemainingTurns() {
+        return remainingTurns;
+    }
+
 }
