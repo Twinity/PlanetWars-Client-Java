540a03f98da5d8c81f0490c79ee2a5a21f8fed11
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 02:21:14 +0430

    Removed Spark and stuff

--- src/com/twinity/PlanetWars/Server.java
+++ src/com/twinity/PlanetWars/Server.java
@@ -4,12 +4,6 @@
 
 package com.twinity.PlanetWars;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
-import spark.Spark;
-
 import java.io.IOException;
 
 public class Server {
@@ -25,12 +19,7 @@ public class Server {
     }
 
     private void setConfigs() {
-        Spark.port(ServerConfig.getPort());
-        // Config Logger
-        BasicConfigurator.configure();
-        // Turn off log4j logs in console
-        Logger.getLogger("org").setLevel(Level.OFF);
-        Logger.getLogger("akka").setLevel(Level.OFF);
+
     }
 
     private void runServer() throws IOException {
