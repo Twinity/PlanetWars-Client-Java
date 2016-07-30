6a853af8ed6c567d420818ea3f66e08bf45408d0
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 00:46:01 +0430

    Added ArmyMovement class with attributes

---
+++ src/com/twinity/PlanetWars/ArmyMovement.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Borderliner on 7/31/2016.
+ */
+
+package com.twinity.PlanetWars;
+
+public class ArmyMovement {
+
+    private int source;
+    private int destination;
+    private int armyCount;
+
+}
