578a6c50ac7515bf70843faf9af14c98d05d7e0f
Borderliner <hajianpour.mr@gmail.com>
2016-07-30 23:44:35 +0430

    Added ServerConfig enum to contain basic server settings

---
+++ src/main/java/ServerConfig.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Borderliner on 7/30/2016.
+ */
+public static enum ServerConfig {
+    private static int _port = 0;
+    private static int _minThreads = 2;
+    private static int _maxThreads = 8;
+    private static int _timeOutMillis = 3000;
+
+    public static int getPort() {
+        return _port;
+    }
+
+    public static int getMinThreads() {
+        return _minThreads;
+    }
+
+    public static int getMaxThreads() {
+        return _maxThreads;
+    }
+
+    public static int getTimeout() {
+        return _timeOutMillis;
+    }
+
+}
