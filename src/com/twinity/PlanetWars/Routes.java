855429a3a1b606360236034615e753c0d741b2d6
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 02:09:04 +0430

    Added getServerState() and sendState()

--- src/com/twinity/PlanetWars/Routes.java
+++ src/com/twinity/PlanetWars/Routes.java
@@ -1,9 +1,41 @@
+/**
+ * Created by Borderliner on 7/31/2016.
+ */
+
 package com.twinity.PlanetWars;
 
-/**
- * Created by Golsa on 7/31/2016.
- */
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import com.google.gson.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 public class Routes {
 
-    
+    public static World getServerState() throws IOException {
+        URL url = new URL("localhost:" + ServerConfig.getServerPort() + "/serverdata");
+        Gson gson = new Gson();
+
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.connect();
+
+        World world = gson.fromJson(request.getContent().toString(), World.class);
+        return world;
+    }
+
+    public static int sendState(ArmyMovement[] inArmyMovement) throws IOException {
+        String url = "localhost:" + ServerConfig.getServerPort() + "/clientdata";
+        Gson gson = new Gson();
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(url);
+        StringEntity postingString = new StringEntity(gson.toJson(inArmyMovement));
+        post.setEntity(postingString);
+        post.setHeader("Content-type", "application/json");
+        HttpResponse response = httpClient.execute(post);
+        return response.getStatusLine().getStatusCode();
+    }
 }
