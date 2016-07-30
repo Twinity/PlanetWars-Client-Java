21aa84ef654f177cf78d9939bdcae0e667ff90d3
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 02:09:26 +0430

    Server now runs user script and sends them to the server

--- src/com/twinity/PlanetWars/Server.java
+++ src/com/twinity/PlanetWars/Server.java
@@ -10,18 +10,46 @@ import org.apache.log4j.Logger;
 
 import spark.Spark;
 
+import java.io.IOException;
+
 public class Server {
 
     public Server() {
         setConfigs();
+        try {
+            runServer();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
     }
 
-    public void setConfigs() {
+    private void setConfigs() {
         Spark.port(ServerConfig.getPort());
         // Config Logger
         BasicConfigurator.configure();
         // Turn off log4j logs in console
         Logger.getLogger("org").setLevel(Level.OFF);
         Logger.getLogger("akka").setLevel(Level.OFF);
+    }
+
+    private void runServer() throws IOException {
+        AI ai = new AI();
+        World world = new World();
+
+        while (true) {
+            world = Routes.getServerState();
+            ArmyMovement[] movementList = ai.doTurn(world);
+            int status = Routes.sendState(movementList);
+            if (status != 200) {
+                System.out.println("Server is not responding...");
+                ServerConfig.setClientRetryTimes(ServerConfig.getClientRetryTimes() - 1);
+            }
+
+            if (ServerConfig.getClientRetryTimes() == 0) {
+                System.out.println("Server is not responding. Shutting down...");
+                break;
+            }
+        }
     }
 }
