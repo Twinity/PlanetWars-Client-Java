45ee41114492f81eefa5df7d989c66999d6a9274
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 02:08:41 +0430

    Sends World object into AI

--- src/com/twinity/PlanetWars/AI.java
+++ src/com/twinity/PlanetWars/AI.java
@@ -1,12 +1,13 @@
+/**
+ * Created by Borderliner on 7/31/2016.
+ */
+
 package com.twinity.PlanetWars;
 
 import java.util.ArrayList;
 
-/**
- * Created by Golsa on 7/31/2016.
- */
 public class AI {
-    public ArmyMovement[] doTurn() {
+    public ArmyMovement[] doTurn(World world) {
 
         ArrayList<ArmyMovement> move = new ArrayList<>();
         move.add(new ArmyMovement(101, 102, 20));
