6a684589f8f4a38aa4828fccbf8d7ad59fde3e92
Borderliner <hajianpour.mr@gmail.com>
2016-08-01 00:05:20 +0430

    Added Getters and Setters to ArmyMovement class for a better control for the user.

--- src/main/com/twinity/PlanetWars/ArmyMovement.java
+++ src/main/com/twinity/PlanetWars/ArmyMovement.java
@@ -16,4 +16,37 @@ public class ArmyMovement {
         this.armyCount = inArmyCount;
     }
 
+    public ArmyMovement() {
+        this.source = 0;
+        this.destination = 0;
+        this.armyCount = 0;
+    }
+
+    public int getSource() {
+        return this.source;
+    }
+
+    public int getDestination() {
+        return this.destination;
+    }
+
+    public int getArmyCount() {
+        return this.armyCount;
+    }
+
+    public void setSource(int inSource) {
+        if (inSource > 0)
+            this.source = inSource;
+    }
+
+    public void setDestination(int inDestination) {
+        if (inDestination > 0)
+            this.destination = inDestination;
+    }
+
+    public void setArmyCount(int inArmyCount) {
+        if (inArmyCount > 0)
+            this.armyCount = inArmyCount;
+    }
+
 }
