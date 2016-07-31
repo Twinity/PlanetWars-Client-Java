be3afc89ea18869471329bf39d4d703a76b65cc7
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 23:58:22 +0430

    Removed unused variables and functions

--- src/main/com/twinity/PlanetWars/ServerConfig.java
+++ src/main/com/twinity/PlanetWars/ServerConfig.java
@@ -5,28 +5,9 @@
 package com.twinity.PlanetWars;
 
 public class ServerConfig {
-    private static int _port = 3001;
-    private static int _minThreads = 2;
-    private static int _maxThreads = 8;
-    private static int _timeOutMillis = 3000;
+
     private static int _serverPort = 4000;
     private static int _clientRetryTimes = 5;
-
-    public static int getPort() {
-        return _port;
-    }
-
-    public static int getMinThreads() {
-        return _minThreads;
-    }
-
-    public static int getMaxThreads() {
-        return _maxThreads;
-    }
-
-    public static int getTimeout() {
-        return _timeOutMillis;
-    }
 
     public static int getServerPort() {
         return _serverPort;
