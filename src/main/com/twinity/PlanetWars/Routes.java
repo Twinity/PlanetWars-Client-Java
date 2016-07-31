4dda5bfcc8df5a4a4c762a623470021084a9a5cf
Borderliner <hajianpour.mr@gmail.com>
2016-07-31 18:23:06 +0430

    Changed POST and GET methods functionality

--- src/main/com/twinity/PlanetWars/Routes.java
+++ src/main/com/twinity/PlanetWars/Routes.java
@@ -4,38 +4,42 @@
 
 package com.twinity.PlanetWars;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.io.*;
 import java.net.URL;
+
+import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.*;
-import org.apache.http.client.HttpClient;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
 
 public class Routes {
 
     public static World getServerState() throws IOException {
         URL url = new URL("http://localhost:" + ServerConfig.getServerPort() + "/serverdata");
-        Gson gson = new Gson();
+        String data = "";
 
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-        request.connect();
+        try {
+            data = HttpRequest.get(url).body();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
-        World world = gson.fromJson(request.getContent().toString(), World.class);
+        World world = new Gson().fromJson(data, World.class);
         return world;
     }
 
-    public static int sendState(ArmyMovement[] inArmyMovement) throws IOException {
-        String url = "http://localhost:" + ServerConfig.getServerPort() + "/clientdata";
-        Gson gson = new Gson();
-        HttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost(url);
-        StringEntity postingString = new StringEntity(gson.toJson(inArmyMovement));
-        post.setEntity(postingString);
-        post.setHeader("Content-type", "application/json");
-        HttpResponse response = httpClient.execute(post);
-        return response.getStatusLine().getStatusCode();
+    public static int sendState(ArmyMovement[] inArmyMovement) {
+        String _url = "http://localhost:" + ServerConfig.getServerPort() + "/clientdata";
+        URL url;
+
+        String jsonToSend = new Gson().toJson(inArmyMovement);
+        int status = 500;
+        try {
+            url = new URL(_url);
+            status = HttpRequest.post(url).send(jsonToSend.getBytes()).code();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return status;
     }
+
 }
